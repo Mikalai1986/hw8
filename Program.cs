5db693d24024a76bea3430692dda2c8e3c2f699f
FostikN <fostik@tut.by>
2023-07-05 13:31:45 +0300

    решение задачи 56

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿//Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы
+﻿/*Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы
 //каждой строки двумерного массива
 int InputNum(string message)
 {
@@ -61,3 +61,83 @@ Print2DArray(myArray);
 Console.WriteLine();
 ChangeRows(myArray);
 Print2DArray(myArray);
+задайте прямоуголный двумерный массив. Напишите программу, которая будет находить строку с наименьшей
+суммой элементов
+*/
+int InputNum(string message)
+{
+    Console.WriteLine(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+int[,] Create2DArray(int rows, int columns)
+{
+    return new int[rows, columns];
+}
+
+void Fill2DArray(int[,] array, int min, int max)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = rnd.Next(min, max + 1);
+}
+int Maxstring(int[,] array)
+{
+    int Sumzero = 0;
+    if (array.GetLength(0) != array.GetLength(1))
+    {
+        Console.WriteLine("Массив  - не прямоугольный");
+        
+    }
+    else
+        for (int i = 0; i < 1; i++)
+        {
+            
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                Sumzero = Sumzero + array[i, j];
+            }
+        }
+    return Sumzero;
+}
+int MinInd(int[,] array, int Sumzero)
+{
+    
+    int Indmin = 0;
+    for (int i = 1; i < array.GetLength(0); i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < array.GetLength(1); j++)
+            sum = sum + array[i, j];
+        if (sum < Sumzero)
+        {
+            Sumzero = sum;
+            Indmin = i;
+        }
+    }
+    return Indmin;
+}
+void Print2DArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i, j]}\t");
+        Console.WriteLine();
+    }
+}
+
+int rows = InputNum("Введите количество строк: ");
+int cols = InputNum("Введите количество столбцов: ");
+
+int minValue = InputNum("Введите минимальное значение элемента: ");
+int maxValue = InputNum("Введите максимальное значение элемента: ");
+
+int[,] myArray = Create2DArray(rows, cols);
+Fill2DArray(myArray, minValue, maxValue);
+Print2DArray(myArray);
+Console.WriteLine();
+int zerosum = Maxstring(myArray);
+int min = MinInd(myArray, zerosum);
+Console.WriteLine($"индекс строки с минимальной суммой значений = { min}");
